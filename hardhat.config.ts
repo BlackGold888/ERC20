836355dce014904a2130bc21096b0ef0bc33b7b3
blackgold888 <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>
2022-01-19 20:15:48 +0200

    Configure hardhat config file

--- hardhat.config.ts
+++ hardhat.config.ts
@@ -15,17 +15,29 @@ dotenv.config();
 // Go to https://hardhat.org/config/ to learn more
 
 const config: HardhatUserConfig = {
-  solidity: "0.8.4",
+  defaultNetwork: "localhost",
+  solidity: {
+    version: "0.8.4",
+    settings: {
+      optimizer: {
+        enabled: true,
+        runs: 100
+      }
+    }
+  },
   networks: {
     ropsten: {
-      url: process.env.ROPSTEN_URL || "",
+      url: process.env.ROPSTEN_URL || "localhost",
       accounts:
         process.env.PRIVATE_KEY !== undefined ? [process.env.PRIVATE_KEY] : [],
     },
   },
   gasReporter: {
-    enabled: process.env.REPORT_GAS !== undefined,
-    currency: "USD",
+    enabled: (process.env.REPORT_GAS) ? true : true,
+    token: 'ETH',
+    onlyCalledMethods: true,
+    showTimeSpent: true,
+    gasPriceApi: 'Etherscan'
   },
   etherscan: {
     apiKey: process.env.ETHERSCAN_API_KEY,
